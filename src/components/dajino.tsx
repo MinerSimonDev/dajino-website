f2dfece6e6e57569cdc55b0d6e980a322ba98c7b
MinerSimonDev <simon8502x@gmail.com>
2024-10-21 22:01:46 +0200

    finished website

--- src/components/dajino.tsx
+++ src/components/dajino.tsx
@@ -9,6 +9,9 @@ export default function dajino(props: Props) {
             <div className="dajino-container-1" id='about'>
                 <span className='h1'>DAJINO</span>
                 <span className='h3'>DJ @ SHAKE CLUBBING</span>
+                <span className='h3 book-dajino-now'>
+                    <a href='mailto:bookingsdajino@gmail.com'>BOOK DAJINO NOW</a>
+                </span>
             </div>
         </div>
     )
