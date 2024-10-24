a8d5140ac849967ec286e940a4224973e1cb7bd7
MinerSimonDev <simon8502x@gmail.com>
2024-10-24 18:45:55 +0200

    payload cms

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import Navbar from './components/navbar.tsx';
 import Dajino from './components/dajino.tsx';
 import Gigs from './components/gigs.tsx';
 import Impressum from './components/impressum.tsx';
+import React, { useRef } from 'react';
+
+import { motion, useInView } from 'framer-motion';
 
 function App() {
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   return (
     <div className="App">
       <Navbar />
       <Dajino />
-      <Gigs
-        path1='/images/dajino1.jpg'
-        path2='/images/dajino2.jpg'
-        path3='/images/dajino3.jpg'
-      />
-      <Impressum/>
+      <motion.div
+        ref={ref}
+        initial={{ y: '-15vh', opacity: 0 }}
+        animate={isInView ? { y: 0, opacity: 1 } : {}}
+        transition={{ type: 'spring', stiffness: 50, duration: 1.5 }}
+      >
+        <Gigs
+          path1='/images/dajino1.jpg'
+          path2='/images/dajino2.jpg'
+          path3='/images/dajino3.jpg'
+        />
+      </motion.div>
+      <Impressum />
     </div>
   );
 }
